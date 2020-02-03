7fcc36af6ff38e4160dba713251fdbdb20d846e3
oscar <oscarnetworkingpro@gmail.com>
2020-02-03 10:36:09 -0500

    Fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,42 +1,6 @@
 #include "sort.h"
 
 /**
- * partition - Function to sort inside the partition
- * and to point to the new pivot
- * @array: array to be sorted
- * @lo: lowest index of the array
- * @hi: jighest index of the array
- * @size: size of the array
- * Return: index of the pivot
- */
-size_t partition(int *array, int lo, int hi, size_t size)
-{
-	int pivot = array[hi];
-	int i = lo;
-	int j;
-	int temp;
-
-	size = size;
-	for (j = lo; j < hi; j++)
-	{
-		if (array[j] < pivot)
-		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			if (i != j)
-				print_array(array, size);
-			i += 1;
-		}
-	}
-	temp = array[i];
-	array[i] = array[hi];
-	array[hi] = temp;
-	if (i != hi)
-		print_array(array, size);
-	return (i);
-}
-/**
  * quicksort - Function to divide the array
  * after the new position of the pivot is returned
  * @array: array to be sorted
@@ -46,13 +10,32 @@ size_t partition(int *array, int lo, int hi, size_t size)
  */
 void quicksort(int *array, int lo, int hi, size_t size)
 {
-	int piv;
+	int pivot = array[hi];
+	int i = lo;
+	int j;
+	int temp;
 
 	if (lo < hi)
 	{
-		piv = partition(array, lo, hi, size);
-		quicksort(array, lo, piv - 1, size);
-		quicksort(array, piv + 1, hi, size);
+		for (j = lo; j < hi; j++)
+		{
+			if (array[j] < pivot)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				if (i != j)
+					print_array(array, size);
+				i += 1;
+			}
+		}
+		temp = array[i];
+		array[i] = array[hi];
+		array[hi] = temp;
+		if (i != hi)
+			print_array(array, size);
+		quicksort(array, lo, i - 1, size);
+		quicksort(array, i + 1, hi, size);
 	}
 }
 /**
