3fe7e804848ad397acaf85ad5b8fce73f80bb5cf
shincap8 <shincap8@gmail.com>
2020-02-03 14:18:27 +0000

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts an array
+ * of integers in ascending order using the Bubble sort algorithm
+ * @list: list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL, *index = NULL, *before = NULL;
+
+	if (*list == NULL)
+		return;
+	current = *list;
+	while (current->next != NULL)
+	{
+		index = current->next;
+		if (current->n > index->n)
+		{
+			before = current;
+			while (before != NULL)
+			{
+				if (before->n > index->n)
+				{
+					if (before->prev == NULL)
+						index->next->prev = before, *list = index;
+					if (index->next == NULL)
+						before->prev->next = index;
+					if (index->next != NULL && before->prev != NULL)
+						index->next->prev = before, before->prev->next = index;
+					before->next = index->next;
+					index->prev = before->prev;
+					before->prev = index, index->next = before;
+					before = index->prev, print_list(*list);
+				}
+				else
+					before = before->prev;
+			}
+		}
+		else
+			current = current->next;
+	}
+}
